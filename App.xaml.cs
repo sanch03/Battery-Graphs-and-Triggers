b6a50a56f8706bf5904a4622068d432c32726c39
Sanchit <sanchitormoney@gmail.com>
2020-02-03 10:30:49 +1100

    Initial Commit

---
+++ App.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Windows.System.Power;
+
+namespace Battery_Monitor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private System.Windows.Forms.NotifyIcon _notifyIcon;
+        private bool _isExit;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            string str = PowerManager.RemainingChargePercent.ToString();
+            Font fontToUse = new Font("Microsoft Sans Serif", 16, System.Drawing.FontStyle.Regular, GraphicsUnit.Pixel);
+            Brush brushToUse = new SolidBrush(Color.White);
+            Bitmap bitmapText = new Bitmap(16, 16);
+            Graphics g = System.Drawing.Graphics.FromImage(bitmapText);
+
+            IntPtr hIcon;
+
+            g.Clear(Color.Transparent);
+            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SingleBitPerPixelGridFit;
+            g.DrawString(str, fontToUse, brushToUse, -4, -1);
+            hIcon = (bitmapText.GetHicon());
+            
+            //DestroyIcon(hIcon.ToInt32);
+
+            base.OnStartup(e);
+            MainWindow = new MainWindow();
+            MainWindow.Closing += MainWindow_Closing;
+
+            _notifyIcon = new System.Windows.Forms.NotifyIcon();
+            _notifyIcon.Click += (s, args) => ShowMainWindow();
+             _notifyIcon.Icon = System.Drawing.Icon.FromHandle(hIcon);
+            //_notifyIcon.Icon = Battery_Monitor.Properties.Resources.ea93_Sm6_icon;
+            _notifyIcon.Visible = true;
+
+            CreateContextMenu();
+            PowerManager.RemainingChargePercentChanged += ChargeChange;
+
+        }
+        public void ChargeChange(object sender, object e)
+        {
+            string str = PowerManager.RemainingChargePercent.ToString();
+            Font fontToUse = new Font("Microsoft Sans Serif", 16, System.Drawing.FontStyle.Regular, GraphicsUnit.Pixel);
+            Brush brushToUse = new SolidBrush(Color.White);
+            Bitmap bitmapText = new Bitmap(16, 16);
+            Graphics g = System.Drawing.Graphics.FromImage(bitmapText);
+
+            IntPtr hIcon;
+
+            g.Clear(Color.Transparent);
+            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SingleBitPerPixelGridFit;
+            g.DrawString(str, fontToUse, brushToUse, -4, -1);
+            hIcon = (bitmapText.GetHicon());
+            _notifyIcon.Icon = System.Drawing.Icon.FromHandle(hIcon);
+        }
+        public void CreateContextMenu()
+        {
+            _notifyIcon.ContextMenuStrip =
+              new System.Windows.Forms.ContextMenuStrip();
+            _notifyIcon.ContextMenuStrip.Items.Add("MainWindow...").Click += (s, e) => ShowMainWindow();
+            _notifyIcon.ContextMenuStrip.Items.Add("Exit").Click += (s, e) => ExitApplication();
+        }
+
+        private void ExitApplication()
+        {
+            _isExit = true;
+            MainWindow.Close();
+            _notifyIcon.Dispose();
+            _notifyIcon = null;
+        }
+
+        private void ShowMainWindow()
+        {
+            if (MainWindow.IsVisible)
+            {
+                if (MainWindow.WindowState == WindowState.Minimized)
+                {
+                    MainWindow.WindowState = WindowState.Normal;
+                }
+                MainWindow.Activate();
+            }
+            else
+            {
+                MainWindow.Show();
+                MainWindow.Activate();
+            }
+        }
+
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if (!_isExit)
+            {
+                e.Cancel = true;
+                MainWindow.Hide(); // A hidden window can be shown again, a closed one not
+            }
+        }
+        public void test()
+        {
+            MessageBox.Show("hi");
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Windows.System.Power;
+using Windows.Graphics.Display;
+
+namespace Battery_Monitor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+               
+                public MainWindow()
+        {
+           
+            InitializeComponent();
+            Vis.counter = 0;
+            PowerManager.RemainingChargePercentChanged += PowerManager_RemainingChargePercentChanged;
+            
+        }
+                static class Vis
+        {
+            public static int counter;
+        }
+             private void Window_Loaded(object sender, EventArgs e)
+        {
+            Vis.counter = 0;
+           // System.Windows.MessageBox.Show("Start");
+            PowerStatus status = SystemInformation.PowerStatus;
+           //percentage.Text = status.BatteryLifePercent.ToString("P0");
+            //timeremaining.Text = status.BatteryLifeRemaining.ToString();
+            var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+            this.Left = desktopWorkingArea.Right - this.Width;
+            this.Top = desktopWorkingArea.Bottom - this.Height;
+            //RefreshStatus;
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += RefreshStatus;
+            timer.Start();
+            
+        }
+        void Window_Deactivated(object sender, EventArgs e)
+        {
+
+            this.Close();
+            Vis.counter = 1;
+            
+        }
+
+        void RefreshStatus(object sender, EventArgs e)
+        {
+            if(Vis.counter == 1)
+            {
+                DispatcherTimer timer = (DispatcherTimer)sender;
+                timer.Stop();
+               // System.Windows.MessageBox.Show("Stop");
+              
+            }
+            powerrefresh();
+            
+        }
+
+        private void PowerManager_RemainingChargePercentChanged(object sender, object e)
+        {
+            // System.Windows.MessageBox.Show("hi");
+            bool c = bool.Parse("false");
+            if (IsActiveProperty.Equals(c))
+            {
+                System.Windows.MessageBox.Show("hey");
+                powerrefresh();
+            }
+            
+ 
+        }
+        public void powerrefresh()
+        {
+            PowerStatus power = SystemInformation.PowerStatus;
+
+            // Battery Remaining
+            int secondsRemaining = power.BatteryLifeRemaining;
+            if (secondsRemaining >= 0)
+            {
+                TimeSpan remaining = TimeSpan.FromSeconds(secondsRemaining);
+                if (remaining.TotalHours < 1)
+                {
+                    timeremaining.Text = remaining.Minutes + " minutes";
+                }
+                else
+                {
+                    timeremaining.Text = string.Format("{0:00}h {1:00}m", (int)remaining.TotalHours, remaining.Minutes);
+                }
+                //timeremaining.Text = string.Format("{0} min", secondsRemaining / 60);
+                //timeremaining.Text = string.Format("{0} sec", secondsRemaining);
+            }
+            else
+            {
+                timeremaining.Text = string.Empty;
+            }
+
+            // Battery Status
+            // percentage.Text = power.BatteryChargeStatus.ToString();
+
+            percentage.Text = PowerManager.RemainingChargePercent.ToString();
+        }
+
+    }
+}
